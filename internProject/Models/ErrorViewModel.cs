d2ad9923ec082b34de70d6f0ecff4801c2c71c1d
Hoang Hai <nhh09012001@gmail.com>
2024-09-05 10:25:44 +0700

    initial-commit

---
+++ internProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace internProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
